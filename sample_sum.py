dedce855627313ab324378ace4b59eefc730835c
OmWhoCodex <omkadane5@gmail.com>
2025-08-12 18:44:40 +0530

    Feat: Update sample_sum.py to check deployment error

--- sample_sum.py
+++ sample_sum.py
@@ -2,4 +2,4 @@ a = int(input("Enter the 1st number: "))
 b = int(input("Enter the 2nd number: "))
 sum = a + b
 
-printf(f"Sum of the two numbers is: {sum}")
+printf(f"Sum of the two numbers is: {sum")
